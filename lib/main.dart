5ae75e763710c7db068140294bb1b21576a52104
malbogha <malbogha@gmail.starways.com.sa>
2025-07-01 09:41:41 +0300

    test 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'audio_app_manager/audio_app_manager_demo.dart';
@@ -7,7 +8,9 @@ import './agentic_app_manager/agentic_app_manager_demo.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  if (kDebugMode) {
+    print("test");
+  }
   runApp(const AgenticAppManagerDemo());
   // runApp(AudioAgenticAppManagerDemo());
 }
